0c533b20238ed19c3008e246bd46964017717e3b
stack.peter21 <stackpeter21@gmail.com>
2022-07-08 12:32:54 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const { Router } = require('express')
+
+const ContactController = require('./app/controllers/ContactController');
+const CategoriesController = require('./app/controllers/CategoriesController');
+
+const router = Router();
+
+router.get('/contacts', ContactController.index)
+router.get('/contacts/:id', ContactController.show)
+router.delete('/contacts/:id', ContactController.delete)
+router.post('/contacts', ContactController.store)
+router.put('/contacts/:id', ContactController.update)
+
+router.get('/categories', CategoriesController.index)
+router.post('/categories', CategoriesController.store)
+
+module.exports = router;
